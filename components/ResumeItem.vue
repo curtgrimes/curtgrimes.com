201b34b3e6ef4e5932d4647ba9b4b7e93e8e188d
Curt Grimes <curt@curtgrimes.com>
2019-11-06 07:44:05 -0600

    Fix resume responsiveness on mobile

--- components/ResumeItem.vue
+++ components/ResumeItem.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="row no-gutters mb-3">
     <div
-      class="col-pr-2 col-md-3 col-xl-2 small text-right pr-4 d-flex d-md-block mb-2 mb-md-0"
+      class="col-pr-2 col-md-3 col-xl-2 small pr-4 d-flex d-md-block mb-2 mb-md-0 text-sm-right"
       v-if="$slots.dates || $slots.company || $slots.location"
     >
       <span v-if="$slots.company">
         <slot name="company"></slot>
       </span>
-      <span class="ml-auto d-md-block" v-if="$slots.dates || $slots.location">
+      <span class="ml-auto d-md-block text-right" v-if="$slots.dates || $slots.location">
         <span class="resume-dates d-md-block" v-if="$slots.dates">
           <slot name="dates"></slot>
         </span>
         <span class="d-md-none text-muted" v-if="$slots.dates && $slots.location">&middot;</span>
-        <span class="resume-location d-md-block" v-if="$slots.location">
+        <span class="resume-location d-md-block" style="white-space:nowrap" v-if="$slots.location">
           <slot name="location"></slot>
         </span>
       </span>
