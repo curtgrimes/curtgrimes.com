1b2482f438ca75b92e12c65e00538543d2cf9c93
Curt Grimes <curt@curtgrimes.com>
2019-11-19 22:19:14 -0600

    Use vue-meta (already included with Nuxt) for including jsonld markup instead of nuxt-jsonld

--- pages/index.vue
+++ pages/index.vue
@@ -27,59 +27,66 @@ export default {
     Projects,
     Contact
   },
-  jsonld() {
-    return {
-      "@context": "http://schema.org",
-      "@type": "Person",
-      name: "Curt Grimes",
-      givenName: "Curt",
-      familyName: "Grimes",
-      contactPoint: [
-        {
-          "@type": "ContactPoint",
-          contactType: "LinkedIn",
-          identifier: "curtgrimes",
-          url: "https://linkedin.com/in/curtgrimes"
-        },
-        {
-          "@type": "ContactPoint",
-          contactType: "GitHub",
-          identifier: "curtgrimes",
-          url: "https://github.com/curtgrimes"
-        }
-      ],
-      email: "curt@curtgrimes.com",
-      homeLocation: {
-        "@type": "Place",
-        name: "Sycamore, IL",
-        address: {
-          "@type": "PostalAddress",
-          addressLocality: "Sycamore",
-          addressRegion: "IL",
-          postalCode: "60178"
-        }
-      },
-      image: "https://curtgrimes.com/og-image.jpg",
-      jobTitle: "Web Developer",
-      knowsAbout: [].concat(...skills.map(s => s.examples.map(e => e.name))),
-      url: "https://curtgrimes.com",
-      sameAs: [
-        "https://github.com/curtgrimes",
-        "https://linkedin.com/in/curtgrimes"
-      ],
-      worksFor: [
-        {
-          "@type": "Organization",
-          name: "Northern Illinois University",
+  head: {
+    script: [
+      {
+        type: "application/ld+json",
+        json: {
+          "@context": "http://schema.org",
+          "@type": "Person",
+          name: "Curt Grimes",
+          givenName: "Curt",
+          familyName: "Grimes",
+          contactPoint: [
+            {
+              "@type": "ContactPoint",
+              contactType: "LinkedIn",
+              identifier: "curtgrimes",
+              url: "https://linkedin.com/in/curtgrimes"
+            },
+            {
+              "@type": "ContactPoint",
+              contactType: "GitHub",
+              identifier: "curtgrimes",
+              url: "https://github.com/curtgrimes"
+            }
+          ],
+          email: "curt@curtgrimes.com",
+          homeLocation: {
+            "@type": "Place",
+            name: "Sycamore, IL",
+            address: {
+              "@type": "PostalAddress",
+              addressLocality: "Sycamore",
+              addressRegion: "IL",
+              postalCode: "60178"
+            }
+          },
+          image: "https://curtgrimes.com/og-image.jpg",
+          jobTitle: "Web Developer",
+          knowsAbout: [].concat(
+            ...skills.map(s => s.examples.map(e => e.name))
+          ),
+          url: "https://curtgrimes.com",
           sameAs: [
-            "https://niu.edu",
-            "https://twitter.com/niulive",
-            "https://www.facebook.com/NorthernIllUniv/",
-            "https://www.linkedin.com/school/northern-illinois-university/"
+            "https://github.com/curtgrimes",
+            "https://linkedin.com/in/curtgrimes"
+          ],
+          worksFor: [
+            {
+              "@type": "Organization",
+              name: "Northern Illinois University",
+              sameAs: [
+                "https://niu.edu",
+                "https://twitter.com/niulive",
+                "https://www.facebook.com/NorthernIllUniv/",
+                "https://www.linkedin.com/school/northern-illinois-university/"
+              ]
+            }
           ]
         }
-      ]
-    };
+      }
+    ]
   }
 };
 </script>
